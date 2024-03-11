7adda0dedd84be64b7a489de5ac978537ef691ef
seoyh1618 <seoyh1618@gmail.com>
2024-03-12 03:07:25 +0900

    Start SpringBoot

---
+++ DataDamoa/src/main/java/com/datadamoa/demo/DataDamoaApplication.java
@@ -0,0 +1,13 @@
+package com.datadamoa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDamoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataDamoaApplication.class, args);
+	}
+
+}
